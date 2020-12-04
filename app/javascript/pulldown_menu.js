0eecc27b1b02eb26bf08e9da7c86a63bac931594
dainosuke1026 <daiichirou929@gmail.com>
2020-12-04 20:58:57 +0900

    フロント調整
    レスポンシブデザイン

--- app/javascript/pulldown_menu.js
+++ app/javascript/pulldown_menu.js
@@ -1,6 +1,6 @@
 function pulldown_menu() {
   const menuIcon = document.getElementById("open");
-  const pullDownMenu = document.querySelector(".sp-menu > ul");
+  const pullDownMenu = document.querySelector(".pulldown-content > ul");
   menuIcon.addEventListener("click", () => {
     pullDownMenu.classList.toggle("active");
   });
