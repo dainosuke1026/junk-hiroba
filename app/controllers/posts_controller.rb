d5f847468443af7bcde61e0f59317c1fd7ad1aaa
dainosuke1026 <daiichirou929@gmail.com>
2020-12-12 21:07:39 +0900

    チャット作成機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,19 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @post = Post.create(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:name, user_ids:[])
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
   has_many :post_users
   has_many :users, through: :post_users
+
+  validates :name, presence: true
 end
