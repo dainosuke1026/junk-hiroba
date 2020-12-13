e98249dbbd90854b140bb8494840899a12a5fa72
dainosuke1026 <daiichirou929@gmail.com>
2020-12-14 00:50:49 +0900

    メッセージ表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,14 +2,16 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @post = Post.find(params[:post_id])
+    @messages = @post.messages.includes(:user)
   end
-
+  
   def create
     @post = Post.find(params[:post_id])
-    @message = @post.messsages.new(message_params)
+    @message = @post.messages.new(message_params)
     if @message.save
       redirect_to post_messages_path(@post)
     else
+      @messages = @post.messages.includes(:user)
       render :index
     end
   end
