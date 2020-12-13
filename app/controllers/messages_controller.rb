4f15740e807f187537530499a3173e4caa0f0c58
dainosuke1026 <daiichirou929@gmail.com>
2020-12-13 22:49:28 +0900

    メッセージ送信機能

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,22 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
     @post = Post.find(params[:post_id])
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @message = @post.messsages.new(message_params)
+    if @message.save
+      redirect_to post_messages_path(@post)
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
   end
 end
