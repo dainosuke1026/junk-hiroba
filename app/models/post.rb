6345e1eb6c590e1bacb4752bb25667007edb08b3
dainosuke1026 <daiichirou929@gmail.com>
2020-12-20 22:42:49 +0900

    チャットルーム検索機能実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,12 @@ class Post < ApplicationRecord
   has_many :messages
 
   validates :name, presence: true
+
+  def self.search(search)
+    if search != ""
+      Post.where('name LIKE(?)', "%#{search}%").includes(:post_users).order('created_at DESC')
+    else
+      Post.all
+    end
+  end
 end
