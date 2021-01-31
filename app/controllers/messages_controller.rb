204c8ae90a57d6692569b4f69582bb33838dae1f
dainosuke1026 <daiichirou929@gmail.com>
2021-02-01 00:18:05 +0900

    ユーザー名とメッセージ表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,17 +3,20 @@ class MessagesController < ApplicationController
     @message = Message.new
     @post = Post.find(params[:post_id])
     @messages = @post.messages.includes(:user)
+    #@post.messages (アソシエーションが組まれているため、@post.messagesとすることで@postに投稿されたmessageをすべて取得できる)
   end
   
   def create
+    # binding.pry
     @post = Post.find(params[:post_id])
-    @message = @post.messages.new(message_params)
-    if @message.save
-      redirect_to post_messages_path(@post)
-    else
-      @messages = @post.messages.includes(:user)
-      render :index
-    end
+    message = @post.messages.create(message_params)
+    render json:{ message: message }
+    # if @message.save
+    #   redirect_to post_messages_path(@post)
+    # else
+    #   @messages = @post.messages.includes(:user)
+    #   render :index
+    # end
   end
 
   private
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 require("../pulldown_menu")
 require("../tab_menu")
+require("../message")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
